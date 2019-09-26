3888edaf11095557a304e7f3333f610a427d6ecc
ferrylehtinen <ferry.lehtinen@metropolia.fi>
2019-09-26 12:05:29 +0300

    Test push

---
+++ PLANETGAME/Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attractor)
+        {
+            attractor.Attract(myTransform);
+        }
+    }
+}
---
+++ PLANETGAME/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed);  
+    }
+}
