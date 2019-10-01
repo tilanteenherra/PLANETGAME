329ff58ed941837f8cbb63fd0ab88865ab1718b9
joonatri <33051433+joonatri@users.noreply.github.com>
2019-10-01 14:52:23 +0300

    Update UIHealthChangeScript.cs

--- PLANETGAME/Assets/Scripts/StatsScripts/UIHealthChangeScript.cs
+++ PLANETGAME/Assets/Scripts/StatsScripts/UIHealthChangeScript.cs
@@ -5,14 +5,15 @@ namespace StatsScripts
 {
     public class UIHealthChangeScript : MonoBehaviour
     {
-        public PlayerStats stats;
-        public GameObject image;
-        public float asd = 0;
+        private PlayerStats stats;
+        private GameObject image;
+        private float asd = 0;
     
         // Start is called before the first frame update
         void Start()
         {
-        
+            image = GetComponent<GameObject>();
+            stats = GameObject.Find("DemonSlayer").GetComponent<PlayerStats>();
         }
 
         // Update is called once per frame
