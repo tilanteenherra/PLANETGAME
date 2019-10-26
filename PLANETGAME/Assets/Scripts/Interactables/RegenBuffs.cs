ec492297f7daf92ff63c07c4377cd5dbbf6eaf57
joonatri <joonatri@metropolia.fi>
2019-10-26 20:09:50 +0300

    Cactus juice working
    Drinking cactus juice allows player to move faster. working only iin fps controller mode in demonslayer

--- PLANETGAME/Assets/Scripts/Interactables/RegenBuffs.cs
+++ PLANETGAME/Assets/Scripts/Interactables/RegenBuffs.cs
@@ -1,23 +1,52 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Interactables
 {
     public class RegenBuffs : MonoBehaviour
     {
         private PlayerStats stats;
-    
-
+        private GameObject player;
+        private FirstPersonController firstPersonController;
+        
+        private float origSpeed;
+        public float curSpeed;
+        public float cactusJuiceSpeed;
+        private float counterForCactus = 0;
+        private bool cactusPicked;
+        public int cactusPickUpMaxTime;
         public float healthMultiplier;
+        
+        
         // Start is called before the first frame update
         void Start()
         {
+            
+            player = GameObject.Find("DemonSlayer").GetComponent<GameObject>();
+            firstPersonController = GameObject.Find("DemonSlayer").GetComponent<FirstPersonController>();
+            counterForCactus = 0;
+            origSpeed = firstPersonController.walkSpeed;
+            cactusPicked = false;
             stats = GameObject.Find("DemonSlayer").GetComponent<PlayerStats>();
+            curSpeed = origSpeed;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            
+            //Counter for cactus effect to last
+            if (cactusPicked)
+            {
+                counterForCactus += Time.deltaTime;
+                if (counterForCactus >= cactusPickUpMaxTime)
+                {
+                    counterForCactus = 0;
+                    cactusPicked = false;
+                    firstPersonController.walkSpeed = origSpeed;
+                    curSpeed = origSpeed;
+                }
+            }
         }
 
         private void OnTriggerStay(Collider other)
@@ -26,7 +55,18 @@ namespace Interactables
             {
                 stats.curHp += (1 * healthMultiplier);
             }
-    
+            //cactus changes user walk speed from 8 to 15
+            if (other.gameObject.CompareTag("Cactus") && !cactusPicked)
+            {
+                cactusPicked = true;
+                firstPersonController.walkSpeed = cactusJuiceSpeed;
+                curSpeed = cactusJuiceSpeed;
+
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
         }
     }
 }
