1cb83db1996ad1dc1d9669c65b58922ebdbd8162
ferrylehtinen <ferry.lehtinen@metropolia.fi>
2019-11-13 00:18:19 +0200

    Added another gravity attractor for the other planet to fill it with skeletors and benchmark with animations. Why? Hell if i know...

---
+++ PLANETGAME/Assets/Scripts/FauxGravityAttractor1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttractor1 : MonoBehaviour
+{
+    public float gravity1 = -15;
+
+    Transform world1;
+
+    private void Awake()
+    {
+        world1 = GameObject.FindGameObjectWithTag("World1").GetComponent<Transform>();
+    }
+
+    public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity1);
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+}
---
+++ PLANETGAME/Assets/Scripts/FauxGravityBody1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody1 : MonoBehaviour
+{
+
+    public FauxGravityAttractor1 attractor;
+    private Transform myTransform;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        attractor = GameObject.FindGameObjectWithTag("World1").GetComponent<FauxGravityAttractor1>();
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+        /*
+        if (attractor)
+        {
+            attractor.Attract(myTransform);
+        }
+        */
+    }
+}
