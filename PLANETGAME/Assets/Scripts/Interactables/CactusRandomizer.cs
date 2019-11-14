4becf76a985d5ea7e905399036693b718d99495e
joonatri <joonatri@metropolia.fi>
2019-11-14 20:44:46 +0200

    cactusRandomizer not working

---
+++ PLANETGAME/Assets/Scripts/Interactables/CactusRandomizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusRandomizer : MonoBehaviour
+{
+    public GameObject[] cactuses;
+    private GameObject child;
+    public Material cactusMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        child = transform.GetChild(0).gameObject;
+        if (cactuses != null)
+        {
+            GameObject cactus = Instantiate(cactuses[Random.Range(0, cactuses.Length)], child.transform.position,
+                child.transform.rotation,transform);
+            Destroy(child);
+            MeshRenderer asd = cactus.AddComponent<UnityEngine.MeshRenderer>();
+            asd.material = cactusMaterial;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
