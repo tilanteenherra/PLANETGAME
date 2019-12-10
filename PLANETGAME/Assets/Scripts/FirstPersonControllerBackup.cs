58dab7a7f6af692c50c73b3c88cadcf5c57305c6
ferrylehtinen <ferry.lehtinen@metropolia.fi>
2019-12-10 23:25:51 +0200

    Fixed backup name

--- PLANETGAME/Assets/Scripts/FirstPersonControllerBackup.cs
+++ PLANETGAME/Assets/Scripts/FirstPersonControllerBackup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPersonController : MonoBehaviour
+public class FirstPersonControllerBackup : MonoBehaviour
 {
     public float mouseSensitivityX = 100f;
     //public float mouseSensitivityY = 250f;
